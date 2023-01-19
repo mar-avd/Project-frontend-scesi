f5739fa72ef307c6ced55e43f2b33153421c3084
Mariana <marivalleandrea@gmail.com>
2023-01-19 14:00:59 -0400

    Conectando api para ver la nota

--- src/components/NoteModal/NoteModal.js
+++ src/components/NoteModal/NoteModal.js
@@ -2,33 +2,56 @@
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
+import { api } from '../../config/site.config';
+import AuthService from '../../config/auth.service';
 
 export default function NoteModal({idNote}){
     
     const [show, setShow] = useState(false);
+    const [note, setNote] = useState({});
+    const user = AuthService.getCurrentUser();
+    const config = {
+      headers: { Authorization: `Bearer ${user.token}` },
+    };
+    const loadNote = () => {
+      api.get('note/oneNote?noteID=' + idNote, config).then((response) => {
+        setNote(response.data)
+      }).catch((error) => console.log(error))
 
+    }
     //states
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleShow = () => {
+      loadNote();
+      setShow(true)};
     //render
     return(<>
     <Button variant='btn' onClick={handleShow}>
-        Ver
+        Ver nota
       </Button>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Editar</Modal.Title>
+          <Modal.Title>{note.titleNote}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>Estás seguro de eliminar esta nota?</p>
+          <p>{note.contentNote}</p>
+          <div>
+            <span className='badge text-bg-primary'>Etiqueta 1</span>
+          </div>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Eliminar
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Editar
-          </Button>
+          <div className='d-flex justify-content-between'>
+            <p>Ultima modificación {note.modificationDate}</p>
+            <div>
+              <Button variant="secondary" size='sm' onClick={handleClose}>
+                Eliminar
+              </Button>
+              <Button variant="primary" size='sm' onClick={handleClose}>
+                Editar
+              </Button>
+
+            </div>
+          </div>
         </Modal.Footer>
       </Modal>
     </>)
