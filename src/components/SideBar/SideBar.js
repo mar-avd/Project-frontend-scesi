8b05d7928622e103dc1091ece3401c075f64903d
Mariana <marivalleandrea@gmail.com>
2023-01-21 22:52:31 -0400

    Fix: estilo de Sidebar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,56 +1,71 @@
-import { useDispatch } from "react-redux";
-import { Link, useLocation } from "react-router-dom";
-import authService from "../../config/auth.service";
+import { useDispatch } from 'react-redux';
+import { Link, useLocation } from 'react-router-dom';
+import authService from '../../config/auth.service';
 import authAction from '../../redux/auth/actions';
 
-export default function SideBar({sideBarOptions}){
-    let location = useLocation();
+export default function SideBar({ sideBarOptions }) {
+  let location = useLocation();
 
-    const dispatch = useDispatch();
-    const {logout}=authAction;
-    const user = authService.getCurrentUser();
-    //render
-    return(
-        <div>
-            <div className=" container">
-            <div className="d-flex flex-column flex-shrink-0 p-2 text-bg-dark">
-              <a
-                href="/"
-                className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
-              >
-                <span className="fs-4">Notes</span>
-              </a>
-              <hr />
+  const dispatch = useDispatch();
+  const { logout } = authAction;
+  const user = authService.getCurrentUser();
+  //render
+  return (
+    <div className="">
+      <div className="row">
+        <div className="d-flex flex-column flex-shrink-0 p-2 text-bg-dark">
+          <div className="col container">
+            <a
+              href="/"
+              className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+            >
+              <span className="fs-4">Notes</span>
+            </a>
+            <hr />
+          </div>
+          <div className="row ">
+            <div className="col">
               <ul className="nav nav-pills flex-column mb-auto">
                 {sideBarOptions.map((sideBarOption, index) => (
-                    <li className="nav-item" key={index}>
-                    <Link className={"nav-link " + (sideBarOption.to===location.pathname ? 'active' : '')} to={sideBarOption.to}>
-                        <i className={sideBarOption.icon}></i> {sideBarOption.option}
+                  <li className="nav-item" key={index}>
+                    <Link
+                      className={
+                        'nav-link ' + (sideBarOption.to === location.pathname ? 'active' : '')
+                      }
+                      to={sideBarOption.to}
+                    >
+                      <i className={sideBarOption.icon}></i> {sideBarOption.option}
                     </Link>
-                    </li>
+                  </li>
                 ))}
               </ul>
               <hr />
-              <div className="dropdown">
-                <a
-                  href="/"
-                  className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <i className="bi bi-person-circle"></i> &nbsp;
-                  <strong>{user.usernameID}</strong>
-                </a>
-                <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-                  <li>
-                    <button className="dropdown-item" onClick={() => {dispatch(logout())}}>
-                      Cerrar sesión
-                    </button>
-                  </li>
-                </ul>
-              </div>
             </div>
           </div>
+          <div className="dropdown">
+            <button
+              className="d-flex align-items-center btn text-white dropdown-toggle"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <i className="bi bi-person-circle"></i> &nbsp;
+              <strong>{user.usernameID}</strong>
+            </button>
+            <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
+              <li>
+                <button
+                  className="dropdown-item"
+                  onClick={() => {
+                    dispatch(logout());
+                  }}
+                >
+                  Cerrar sesión
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
